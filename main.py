1314b6fb3e5083cbb63c06a59e6904c18b47e4ff
saeedsai <saeedmohammadsalehi@gmail.com>
2020-12-19 17:12:14 +0100

    example

---
+++ main.py
@@ -0,0 +1,22 @@
+#This is my comments
+"""This is 
+more comments"""
+x=5
+y="XXX"
+c1= 2-1j
+import random
+
+print("start of program")
+print(y)
+def myfun():
+    if (x>2):
+        y="complex ="
+        global z 
+        z="ZZZ"
+        print("length: "+str(len(y))+" | "+str(c1+x-1j-4.2)+" | random = "+ str(random.randrange(1, 10)))
+
+myfun()
+
+for i in str(str(z)+y):
+    print(i+".")
+print("program ends here")
